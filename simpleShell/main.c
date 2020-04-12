a92b43557839117c1c77976bd3ff5bd8bb4a53cf
Viktor Klíma <43376119+vecnehladny@users.noreply.github.com>
2020-04-12 10:23:10 +0200

    Update main.c

--- simpleShell/main.c
+++ simpleShell/main.c
@@ -12,6 +12,17 @@
 #include <unistd.h>
 #include <string.h>
 
+void type_prompt(){
+    static int first_time = 1;
+    if (first_time) {
+        const char* CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J]";
+        write(STDOUT_FILENO,CLEAR_SCREEN_ANSI,12);
+        first_time = 0;
+    }
+    
+    printf("#");
+}
+
 int main(int argc, const char * argv[]) {
     char cmd[100], command[100], *parameters[20];
          
