98c3483b92709026d84d3169aec1875cb3ed42c7
Viktor Klíma <43376119+vecnehladny@users.noreply.github.com>
2020-04-12 10:11:07 +0200

    Initial commit

--- simpleShell/main.c
+++ simpleShell/main.c
@@ -7,6 +7,10 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
