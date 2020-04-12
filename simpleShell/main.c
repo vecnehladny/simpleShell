b51babfa45d4e4d081db4705635861fbcc14abdd
Viktor Klíma <43376119+vecnehladny@users.noreply.github.com>
2020-04-12 11:56:30 +0200

    pridany cas, username, hostname do promptu

--- simpleShell/main.c
+++ simpleShell/main.c
@@ -9,18 +9,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-void type_prompt(){
-    static int first_time = 1;
-    if (first_time) {
-        const char* CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J]";
-        write(STDOUT_FILENO,CLEAR_SCREEN_ANSI,12);
-        first_time = 0;
+void read_command(char cmd[], char *par[]){
+    char line[1024];
+    int count = 0, i = 0, j = 0;
+    char *array[100], *pch;
+    
+    for(;;){
+        int c = fgetc(stdin);
+        line[count++] = (char) c;
+        
+        if(c == '\n') {
+            break;
+        }
     }
     
-    printf("#");
+    if(count == 1) {
+        return;
+    }
+    
+    pch = strtok(line, " \n");
+    
+    while(pch != NULL) {
+        array[i++] = strdup(pch);
+        pch = strtok(NULL, " \n");
+    }
+    
+    strcpy(cmd, array[0]);
+    
+    for(int j = 0; j < i; j++){
+        par[j] = array[j];
+    }
+    par[i] = NULL;
+}
+
+void type_prompt(){
+    char time_buffer[40];
+    char username_buffer[40];
+    char hostname_buffer[40];
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    strftime(time_buffer, 40, "%T", localtime(&time.tv_sec));
+    getlogin_r(username_buffer, 40);
+    gethostname(hostname_buffer, 40);
+    
+    char prompt[60];
+    strcat(prompt, time_buffer);
+    strcat(prompt, " ");
+    strcat(prompt, username_buffer);
+    strcat(prompt, "@");
+    strcat(prompt, hostname_buffer);
+    strcat(prompt, " > ");
+    
+    
+    
+    write(STDOUT_FILENO,prompt,strlen(prompt));
+ 
 }
 
 int main(int argc, const char * argv[]) {
