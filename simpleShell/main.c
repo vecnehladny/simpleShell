3bca7403fd53599185b0a6352e70c26091565610
Viktor Klíma <43376119+vecnehladny@users.noreply.github.com>
2020-04-12 10:03:13 +0200

    Initial Commit

---
+++ simpleShell/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simpleShell
+//
+//  Created by vecnehladny on 12/04/2020.
+//  Copyright © 2020 vecnehladny. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
